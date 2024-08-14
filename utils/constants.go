67800caaa736cf0d29716414cf93bf9198e75450
Alwin James <38123613+jamealwi2@users.noreply.github.com>
2024-08-13 23:11:16 -0700

    Create constants.go

---
+++ utils/constants.go
@@ -0,0 +1,17 @@
+package utils
+
+const (
+	IMAGE_STATUS_ALL_SAME      = string("100")
+	IMAGE_STATUS_DIFF_APPS     = string("101")
+	IMAGE_STATUS_DIFF_CLUSTERS = string("102")
+	CLUSTERS                   = string("clusters")
+	IMAGE_STATUS               = string("image-status")
+	NOTIFIED_AT                = string("notified-at")
+	LOG_LEVEL                  = string("LOG_LEVEL")
+	CLUSTER_CONTEXT            = string("CLUSTER_CONTEXT")
+	BOT_NAME                   = string("Version Checker")
+	BOT_AVATAR                 = string("<URL>")
+	SLACK_CHANNEL              = string("test-automations")
+	NAMESPACES                 = string("NAMESPACES")
+	DEFAULT_NAMESPACE          = string("default")
+)
